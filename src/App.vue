bb70b127b07cfbfe8593b6aaf3f2fcbe7526fcb7
Bill Dafeldecker <billdafeldecker@gmail.com>
2023-11-29 20:10:10 -0500

    cleaned up and made better randomness.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,3 @@
-<script setup>
-</script>
-
 <template> 
 <ul>
   <li v-for="pair in goodPairs" >
@@ -13,162 +10,145 @@
 </style>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-const FamiliesOriginal = [
-    {
-      Jokers: {
-        Dave: false,
-        Kate: false
-      },
-      SecretSantas: {
-        Victoria: false,
-        Tyler: false
-      }
-    },
-    {
-      Jokers: {
-        Bill: false,
-        Lindsay: false
-      },
-      SecretSantas: {
-        Holly: false,
-        Cora: false,
-        Lana: false
-      }
-    },
-    {
-      Jokers: {
-        Corine: false,
-        Mike: false
-      },
-      SecretSantas: {
-        Jace: false,
-        Madison: false,
-        Dakota: false
-      }
-    },
-    {
-      Jokers: {
-        Sam: false,
-        Brian: false,
-        BrianJr: false
-      },
-      SecretSantas: {
-        Ava: false,
-        Evan: false,
-        Shea: false
-      }
-    },
-    {
-      Jokers: {
-        Steph: false,
-      },
-      SecretSantas: {
-        Rae: false
-      }
-    },
-    {
-      Jokers: {
-        Jean: false,
-      }
-    },
-    {
-      Jokers: {
-        AuntEllen: false,
-      }
-    }
-];
-
-let Families = FamiliesOriginal;
-let totalNumFamilies = Families.length;
-let typeCounts = {
-    Jokers: 0,
-    SecretSantas: 0
-};
-let pairs = [];
-const goodPairs = ref([]);
-
-async function startPairing(){
-    await getPairings();
-}
-async function getPairings(){
-    Families = JSON.parse(JSON.stringify(FamiliesOriginal));
-    console.log(Families)
-    let typeCounts = {
-        Jokers: 0,
-        SecretSantas: 0
-    };
-    let pairs = [];
-    try {
-        Families.forEach((familyObj, familyIndex) => {
-            Object.entries(familyObj).forEach(typeObj => {
-                const [type, typ] = typeObj;
-                Object.entries(typ).forEach(async memberObj => {
-                    const [member, hasPair] = memberObj;
-                    let memberToPair = await findPair(familyIndex, type);
-                    if(memberToPair == "no eligible") return
-                    goodPairs.value.push(`${member} has ${memberToPair}`)
-                    typeCounts[type]++;
-                });
-            });
-        });
-        console.log(await goodPairs.value.length)
-        if(await goodPairs.value.length != 24){
-            setTimeout(async () => {
-              goodPairs.value.length = 0;
-                console.log("Bad Pairing",pairs,typeCounts);
-                await getPairings();
-            }, "50");
+  import { ref, onMounted } from 'vue'
+  const FamiliesOriginal = [
+      {
+        Jokers: {
+          Dave: false,
+          Kate: false
+        },
+        SecretSantas: {
+          Victoria: false,
+          Tyler: false
         }
-        else {
-            console.log("Good", pairs)
+      },
+      {
+        Jokers: {
+          Bill: false,
+          Lindsay: false
+        },
+        SecretSantas: {
+          Holly: false,
+          Cora: false,
+          Lana: false
         }
-    }
-    catch (e) {
-        console.log("getPairings caught")
-    }
-}
-async function findPair(index, type){
-    try {
-        let membersEligibleToGift = [];
-        Families.forEach((familyObj, familyIndex) => {
-            if(familyIndex != index){
-                Object.entries(familyObj).forEach(typeObj => {
-                    const [typeName, typeValue] = typeObj;
-                    if(typeName == type){
-                        Object.entries(typeValue).forEach(memberObj => {
-                            const [member, hasPair] = memberObj;
-                            if(!hasPair){
-                                membersEligibleToGift.push({
-                                    Name: member,
-                                    FamilyIndex: familyIndex,
-                                    Type: typeName
-                                })
-                            }
-                        });
-                    }
-                });
-            }
-        });
-        if(membersEligibleToGift.length > 0){
-            let member = membersEligibleToGift[Math.floor(Math.random() * (membersEligibleToGift.length - 1))];
-            Families[member.FamilyIndex][member.Type][member.Name] = true;
-            return member.Name;
+      },
+      {
+        Jokers: {
+          Corine: false,
+          Mike: false
+        },
+        SecretSantas: {
+          Jace: false,
+          Madison: false,
+          Dakota: false
         }
-        return "no eligible"
-    }
-    catch(e){
-        console.log("findPair caught: ", e);
-    }
-}
-function getRandomFamilyNum(index){
-    let randomFamilyIndex = Math.floor(Math.random() * totalNumFamilies);
-    if(index == randomFamilyIndex) return getRandomFamilyNum(index);
-    return randomFamilyIndex;
-}
+      },
+      {
+        Jokers: {
+          Sam: false,
+          Brian: false,
+          BrianJr: false
+        },
+        SecretSantas: {
+          Ava: false,
+          Evan: false,
+          Shea: false
+        }
+      },
+      {
+        Jokers: {
+          Steph: false,
+        },
+        SecretSantas: {
+          Rae: false
+        }
+      },
+      {
+        Jokers: {
+          Jean: false,
+        }
+      },
+      {
+        Jokers: {
+          AuntEllen: false,
+        }
+      }
+  ];
 
-onMounted(async () => {
-  
-  await startPairing();
-})
+  let Families = FamiliesOriginal;
+  let totalNumFamilies = Families.length;
+  let pairs = [];
+  const goodPairs = ref([]);
+
+  async function startPairing(){
+      await getPairings();
+  }
+  async function getPairings(){
+      Families = JSON.parse(JSON.stringify(FamiliesOriginal));
+      let pairs = [];
+      try {
+          Families.forEach((familyObj, familyIndex) => {
+              Object.entries(familyObj).forEach(typeObj => {
+                  const [type, typ] = typeObj;
+                  Object.entries(typ).forEach(async memberObj => {
+                      const [member, hasPair] = memberObj;
+                      let memberToPair = await findPair(familyIndex, type);
+                      if(memberToPair == "no eligible") return
+                      goodPairs.value.push(`${member} has ${memberToPair}`)
+                  });
+              });
+          });
+          console.log(await goodPairs.value.length)
+          if(await goodPairs.value.length != 24){
+              setTimeout(async () => {
+                goodPairs.value.length = 0;
+                  console.log("Bad Pairing",pairs);
+                  await getPairings();
+              }, "50");
+          }
+          else {
+              console.log("Good", pairs)
+          }
+      }
+      catch (e) {
+          console.log("getPairings caught")
+      }
+  }
+  async function findPair(index, type){
+      try {
+          let membersEligibleToGift = [];
+          Families.forEach((familyObj, familyIndex) => {
+              if(familyIndex != index){
+                  Object.entries(familyObj).forEach(typeObj => {
+                      const [typeName, typeValue] = typeObj;
+                      if(typeName == type){
+                          Object.entries(typeValue).forEach(memberObj => {
+                              const [member, hasPair] = memberObj;
+                              if(!hasPair){
+                                  membersEligibleToGift.push({
+                                      Name: member,
+                                      FamilyIndex: familyIndex,
+                                      Type: typeName
+                                  })
+                              }
+                          });
+                      }
+                  });
+              }
+          });
+          if(membersEligibleToGift.length > 0){
+              let member = membersEligibleToGift.reverse()[Math.floor(Math.random() * (membersEligibleToGift.length - 1))];
+              Families[member.FamilyIndex][member.Type][member.Name] = true;
+              return member.Name;
+          }
+          return "no eligible"
+      }
+      catch(e){
+          console.log("findPair caught: ", e);
+      }
+  }
+  onMounted(async () => {
+    await startPairing();
+  })
 </script>
